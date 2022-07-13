9e60ff00789c0baa51a4265c9de665e6c2edcf91
SuhailY14 <109067113+SuhailY14@users.noreply.github.com>
2022-07-13 17:48:19 +0530

    Update email_check.py

--- .github/scm_scripts/email_check.py
+++ .github/scm_scripts/email_check.py
@@ -5,6 +5,4 @@ url = "https://github.com/settings/emails"
 x = re.search("^[a-zA-Z0-9].@infoblox.com$", url)
 session = HTMLSession()
 r = session.get(url)
-r.html.render()
-for re_match in re.finditer(x, r.html.raw_html.decode()):
-    print(re_match.group())
+r.html.render(email)
