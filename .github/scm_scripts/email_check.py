22691de777c5edb6cf413c96f7f6317efa1b9cce
SuhailY14 <109067113+SuhailY14@users.noreply.github.com>
2022-07-13 18:10:15 +0530

    Update email_check.py

--- .github/scm_scripts/email_check.py
+++ .github/scm_scripts/email_check.py
@@ -5,5 +5,5 @@ email = "https://github.com/settings/emails"
 x = re.search("^[a-zA-Z0-9].@infoblox.com$", email)
 session = HTMLSession()
 r = session.get(email)
-email = r.html.find('email', first=True).text
-print(email)
+email = r.html.find('email')
+print('Valid email')
