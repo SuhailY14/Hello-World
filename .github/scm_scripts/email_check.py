bd79755913522c6b2d24b25ee8898cec4c328dab
SuhailY14 <109067113+SuhailY14@users.noreply.github.com>
2022-07-13 18:15:43 +0530

    Update email_check.py

--- .github/scm_scripts/email_check.py
+++ .github/scm_scripts/email_check.py
@@ -3,10 +3,6 @@ from requests_html import HTMLSession
 
 email = "https://github.com/settings/emails"
 x = re.search("^[a-zA-Z0-9].@infoblox.com$", email)
-try:
-  session = HTMLSession()
-  r = session.get(email)
-  email = r.html.find('email')
 if x:
     print ("YES! We have a match!")
 else:
