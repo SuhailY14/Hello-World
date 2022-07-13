bea0937d0443ed0bcde8cd47fd6ed9224b69d1a2
SuhailY14 <109067113+SuhailY14@users.noreply.github.com>
2022-07-13 21:09:03 +0530

    Update email_check.py

--- .github/scm_scripts/email_check.py
+++ .github/scm_scripts/email_check.py
@@ -1,9 +1,39 @@
-import re
-from requests_html import HTMLSession
+#!/usr/bin/python3
 
-email = "https://github.com/settings/emails"
-x = re.search("^[a-zA-Z0-9].@infoblox.com$", email)
-if x:
-    print ("YES! We have a match!")
-else:
-    print ("No match")
+import re
+import requests
+
+
+GITHUB_EMAIL_URL = 'https://github.com/settings/emails'
+
+def check(email_id):
+    
+    headers = {
+        "Authorization": "Basic ",
+        "Accept": "application"
+    }
+
+    try:
+        url = GITHUB_EMAIL_URL
+        x = "^[a-zA-Z0-9].@infoblox.com$"
+        
+        response = requests.request("GET", url, headers=headers)
+        if response.status_code == 200:
+            print("EMAIL Response: ", response.get('key'))
+            return True
+        else:
+            print("EMAIL error occurred: ", response.text)
+            return False
+    except Exception as e:
+        print("EMAIL Exception occurred ", str(e))
+        return False
+
+    return False
+
+
+if __name__ == '__main__':
+    x = "^[a-zA-Z0-9].@infoblox.com$"
+    if (re.search(x, GITHUB_EMAIL_URL)):
+        print("Valid Infoblox Email ID")
+    else:
+        print("Invalid Email ID")
