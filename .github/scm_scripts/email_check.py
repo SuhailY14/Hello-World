1109f464807f8b5d9317a4778e87d48c9f9ff22a
SuhailY14 <109067113+SuhailY14@users.noreply.github.com>
2022-07-18 17:09:19 +0530

    Update email_check.py

--- .github/scm_scripts/email_check.py
+++ .github/scm_scripts/email_check.py
@@ -7,6 +7,29 @@ import os
 from git import Repo
 
 
-COMMITS_TO_PRINT = 5
+GITHUB_EMAIL_URL = 'https://api.github.com/repos/Hello-World/{0}/commits/{1}'
+
+def check(email_id):
+
+    try: 
+        url = 'https://api.github.com/repos/Hello-World/{0}/commits/{1}'
+        f = urlopen(url)
+        content = f.read()
+        commits = json.loads(content.decode('utf-8'))
+        return commits['sha']
+    except:
+        return "ERROR: Unable to retreive latest commit" 
+ 
+    commit = commits_list[1]
+       
+    author = repo.git.show("-s", "--format=Author: %an <%ae>", commit.hexsha)
+    print(author)
+    
+    regex = "^[a-zA-Z0-9].@infoblox.com$"
+    if (re.search(regex, author)):
+        print("Usage: email_check <EMAIL ID> <IS VALID>")
+    else:
+        print(" Invalid Email ID")
+
 
 
