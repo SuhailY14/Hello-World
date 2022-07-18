0aea651dc219bdc216387d3cbaaef20a97e05ea4
SuhailY14 <109067113+SuhailY14@users.noreply.github.com>
2022-07-18 12:38:40 +0530

    Update email_check.py

--- .github/scm_scripts/email_check.py
+++ .github/scm_scripts/email_check.py
@@ -6,30 +6,25 @@ import requests
 from requests_html import HTMLSession
 
 
-GITHUB_EMAIL_URL = 'https://github.com/settings/emails'
+GITHUB_EMAIL_URL = 'https://api.github.com/repos/Hello-World/{0}/commits/{1}'
 
 def check(email_id):
-    
-    headers = {
-        "Authorization": "Basic ",
-        "Accept": "application"
-    }
 
     try: 
         url = 'https://api.github.com/repos/Hello-World/{0}/commits/{1}'
         f = urlopen(url)
         content = f.read()
         commits = json.loads(content.decode('utf-8'))
-        return commits['sha']
+        return commit.author['sha']
     except:
         return "ERROR: Unable to retreive latest commit" 
 
     
 if __name__ == '__main__':
     regex = "^[a-zA-Z0-9].@infoblox.com$"
-    if (re.search(regex, GITHUB_EMAIL_URL)):
+    if (re.search(regex, commit.author)):
         print("Usage: email_check <EMAIL ID> <IS VALID>")
     else:
-        print("Invalid Email ID")
+        print(" Invalid Email ID")
     
     check(GITHUB_EMAIL_URL)
