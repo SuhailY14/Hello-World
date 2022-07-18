39c8ec0b7ee7bc4d621eea08494568f4ac9c0f1c
SuhailY14 <109067113+SuhailY14@users.noreply.github.com>
2022-07-18 17:02:57 +0530

    Update email_check.py

--- .github/scm_scripts/email_check.py
+++ .github/scm_scripts/email_check.py
@@ -3,20 +3,10 @@
 import re
 import sys
 import requests
+import os
 from git import Repo
 
 
-GITHUB_EMAIL_URL = 'https://api.github.com/repos/Hello-World/{0}/commits/{1}'
+COMMITS_TO_PRINT = 5
 
-def check(email_id):
 
-    try: 
-        url = 'https://api.github.com/repos/Hello-World/{0}/commits/{1}'
-        f = urlopen(url)
-        content = f.read()
-        commits = json.loads(content.decode('utf-8'))
-        return commits['sha']
-    except:
-        return "ERROR: Unable to retreive latest commit" 
- 
-    
