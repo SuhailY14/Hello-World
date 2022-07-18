549f1fa72ec98d93715821394d41c531e1bb36b3
SuhailY14 <109067113+SuhailY14@users.noreply.github.com>
2022-07-18 12:09:20 +0530

    Update email_check.py

--- .github/scm_scripts/email_check.py
+++ .github/scm_scripts/email_check.py
@@ -1,13 +1,42 @@
-import git 
-from git import Repo
+#!/usr/bin/python3
 
-my_repo = git.Repo('Hello-World')
+import re
+import sys
+import requests
+from requests_html import HTMLSession
 
-commits_list = list(repo.iter_commits())
 
-for i in range(5):
-    commit = commits_list[i]
+GITHUB_EMAIL_URL = 'https://github.com/settings/emails'
 
-    print(commit.hexsha)
-    print(commit.author)
-    print(commit.committer)
+def check(email_id):
+    
+    headers = {
+        "Authorization": "Basic ",
+        "Accept": "application"
+    }
+
+    try: 
+        url = 'https://github.com/settings/emails'
+        session = HTMLSession()
+        r = session.get('https://github.com/settings/emails')
+        if r == 200:
+            print("EMAIL Response: ", r.get('key'))
+            return True
+        else:
+            print("EMAIL error occurred: ", r.text)
+            return False
+    except Exception as e:
+        print("EMAIL Exception occurred ", str(e))
+        return False
+
+    return False
+
+
+if __name__ == '__main__':
+    regex = "^[a-zA-Z0-9].@infoblox.com$"
+    if (re.search(regex, GITHUB_EMAIL_URL)):
+        print("Usage: email_check <EMAIL ID> <IS VALID>")
+    else:
+        print("Invalid Email ID")
+    
+    check(GITHUB_EMAIL_URL)
