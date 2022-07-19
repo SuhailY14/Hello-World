339ecc285f89c2005e78280365c255ba91792173
SuhailY14 <suhailtals28@gmail.com>
2022-07-19 05:43:01 -0700

    BLDTLS-137 : Too verify for an infoblox email id

--- .github/scm_scripts/email_check.py
+++ .github/scm_scripts/email_check.py
@@ -3,15 +3,14 @@
 import re
 import sys
 import requests
+import sh
 import git
 from git import Repo
 
 def get_latest_commit(repo_path, branch_path):
-    repo = git.Repo(repo_path)
+    repo = sh.git.bake(_cwd= '/mnt/home/sy/Hello-World')
     for ref in repo.refs:
         if ref.path == 'refs/heads/BLDTLS-137':
             print(ref.commit.author.email)
 
 
-if __name__ == '__main__':
-    get_latest_commit('/mnt/home/sy/Hello-World', '')
