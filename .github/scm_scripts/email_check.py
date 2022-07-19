3e97bc389a7b1d0a87cf82e6443efe872f5a8f92
SuhailY14 <109067113+SuhailY14@users.noreply.github.com>
2022-07-19 17:16:46 +0530

    Update email_check.py

--- .github/scm_scripts/email_check.py
+++ .github/scm_scripts/email_check.py
@@ -7,7 +7,7 @@ import sh
 import subprocess
 from git import Repo
 
-def get_lastest_commit(repo_path, branch_path):
+def get_latest_commit(repo_path, branch_path):
     repo = git.Repo(repo_path)
     for ref in repo.refs:
         print(ref.path)
