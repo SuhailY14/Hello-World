5af21c507c5d0b4371fc1a78a6217ad504227183
SuhailY14 <109067113+SuhailY14@users.noreply.github.com>
2022-07-19 16:42:23 +0530

    Update email_check.py

--- .github/scm_scripts/email_check.py
+++ .github/scm_scripts/email_check.py
@@ -9,9 +9,9 @@ from git import Repo
 
 def author(self):
     repo = sh.git.bake(_cwd= '/mnt/home/sy/Hello-World')
-    commits_list = list(repo.iter_commits())
+    commits_list = list(repo.iter_commits(1))
     
-    commit = commits_list[]
+    commit = commits_list[1]
     
     author = repo.git.show("-s", "--format=Author: %an <%ae>", commit.hexsha)    
     print(author)
