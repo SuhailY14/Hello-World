ad278d9e780855374f50f77e3f4a6f2bc4403fc6
SuhailY14 <109067113+SuhailY14@users.noreply.github.com>
2022-07-19 16:13:48 +0530

    Update email_check.py

--- .github/scm_scripts/email_check.py
+++ .github/scm_scripts/email_check.py
@@ -8,10 +8,11 @@ import subprocess
 from git import Repo
 
 def author(self):
-    git = sh.git.bake(_cwd= '/mnt/home/sy/Hello-World')
-    commits_list = list(git.iter_commits())
+    repo = sh.git.bake(_cwd= '/mnt/home/sy/Hello-World')
+    commits_list = list(repo.iter_commits())
     
     for i in range(5):
         commit = commits_list[i]
         
-        print(commit.author)
+        author = repo.git.show("-s", "--format=Author: %an <%ae>", commit.hexsha)
+        print(author)
