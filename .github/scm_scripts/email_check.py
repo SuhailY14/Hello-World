d4088c64499425e6675c73915dcc683edae414cf
SuhailY14 <109067113+SuhailY14@users.noreply.github.com>
2022-07-19 11:51:55 +0530

    Update email_check.py

--- .github/scm_scripts/email_check.py
+++ .github/scm_scripts/email_check.py
@@ -4,8 +4,13 @@ import re
 import sys
 import requests
 import sh
+import subprocess
 from git import Repo
 
 git = sh.git.bake(_cwd= '/mnt/home/sy/Hello-World')
 
-print ("%s" % git)
+def get_git_revisions_hash():
+    hashes = []
+    hashes.append(subprocess.check_output(['git', 'rev-parse', 'HEAD']))
+    hashes.append(subprocess.check_output(['git', 'rev-parse', 'HEAD^']))
+    return hashes
