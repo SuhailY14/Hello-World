488882a094871f5bc83478a4a5b991b429fc75dd
SuhailY14 <109067113+SuhailY14@users.noreply.github.com>
2022-07-20 15:17:20 +0530

    Update pusher_email_check.py

--- .github/scm_scripts/pusher_email_check.py
+++ .github/scm_scripts/pusher_email_check.py
@@ -1,41 +1,35 @@
 #!/usr/bin/python3
 
 import sys
+import re
+import sh
+from git import Repo
+ 
+regex = "^[a-zA-Z0-9].@infoblox.com$"
 
-def check_pusher_email(email):
-    #Check for non-ascii in The Title
-    if not email.isascii():
-        print("Commit title contains non-ascii characters")
-        sys.exit(1)
-
-    #Check Commit Title Format
-    # NIOS-12345 : Some description
-    # NIOS-12345: Some description
-    ts = email.split(‘@‘)
-    if len(ts) < 2:
+def check_pusher_email(self, email):
+    if (re.fullmatch(regex, pusher_email)):
+        print("VALID DOMAIN NAME")
+        sys.exit(0)
+        
+    else:
         print(“pusher email format: <a-zA-Z0-9>@<infoblox.com>”)
         sys.exit(1)
+        
+    repo = sh.git.bake(_cwd= '/mnt/home/sy/Hello-World')
+    commits_list = list(repo.iter_commits(1))
+    
+    commit = commits_list[1]
+    
+    author = repo.git.log( "--pretty='%ae'", "-n 1")
+    print(author.email)
 
-    tile = ts[0].strip()
-    if len(tile) < 3:
-        print("Email ID INVALID: ", head)
-        sys.exit(1)
-
-    #Check JIRA and Version One
-    cats = tile.split(‘.’)
-    if len(cats) != 2:
-        print(“Domain format: infoblox.com“)
-        sys.exit(1)
-
-    print(tile)
-    sys.exit(0)
-
-
+    
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+   pusher_email = author.email 
+   if len(sys.argv) < 2:
         print("Usage: email_check <pusher email>")
         sys.exit(1)
 
     title = sys.argv[1]
-    check_title(title)
-
+    check_pusher_email(email)
