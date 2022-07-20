8ab0f0c8e899cf8f8deab8c4485690b226b6fd29
SuhailY14 <suhailtals28@gmail.com>
2022-07-20 04:58:37 -0700

    BLDTLS-137 : To verify for an infoblox email id

--- .github/scm_scripts/pusher_email_check.py
+++ .github/scm_scripts/pusher_email_check.py
@@ -7,29 +7,28 @@ from git import Repo
  
 regex = "^[a-zA-Z0-9].@infoblox.com$"
 
-def check_author_email(self, email):
+def check_author_email(email, author):
     if (re.fullmatch(regex, author_email)):
         print("VALID DOMAIN NAME")
         sys.exit(0)
         
     else:
-        print("author email format: <a-zA-Z0-9><@infoblox.com>”)
+        print("Author Email ID format: [a-zA-Z0-9].@infoblox.com")
         sys.exit(0)
-        
-    repo = sh.git.bake(_cwd= '/mnt/home/sy/Hello-World')
-    commits_list = list(repo.iter_commits(1))
-     
-    commit = commits_list[1]
-    
-    author = repo.git.log( "--pretty='%ae'", "-n 1")
-    print(author.email)
 
     
 if __name__ == '__main__':
-   author_email = author.email 
-   if len(sys.argv) < 2:
+    repo = sh.git.bake(_cwd= '/mnt/home/sy/Hello-World')
+    commits_list = list(repo.iter_commits())
+
+    commit = commits_list[1]
+
+    author = repo.git.log( "--pretty='%ae'", "-n 1")
+    print(author.email)
+    author_email = author.email 
+    if len(sys.argv) < 2:
         print("Usage: email_check <pusher email>")
         sys.exit(1)
 
-    title = sys.argv[1]
-    check_pusher_email(email)
+    author_email = sys.argv[1]
+    check_author_email(email, author)
