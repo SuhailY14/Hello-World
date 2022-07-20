f06c427352da496b585e1c9cac27b7aa10679f2f
SuhailY14 <109067113+SuhailY14@users.noreply.github.com>
2022-07-20 10:03:46 +0530

    Update email_check.py

--- .github/scm_scripts/email_check.py
+++ .github/scm_scripts/email_check.py
@@ -4,12 +4,17 @@ import re
 import sys
 import sh
 import git
+import os
 from git import Repo
 
-def get_latest_commit(repo_path, branch_path):
-    repo = sh.git.bake(_cwd= '/mnt/home/sy/Hello-World')
-    for ref in repo.refs:
-        if ref.path == 'refs/heads/BLDTLS-137':
-            print(ref.commit.author.email)
+COMMITS_TO_PRINT = 5
+
+
+
+
+#def get_latest_commit(repo_path, branch_path):
+#   for ref in repo.refs:
+ #       if ref.path == 'refs/heads/BLDTLS-137':
+  #          print(ref.commit.author.email)
 
 
