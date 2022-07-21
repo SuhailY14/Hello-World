364c1728475e431cd4d61fda2c431c840e82e98e
SuhailY14 <109067113+SuhailY14@users.noreply.github.com>
2022-07-21 11:27:27 +0530

    Update pusher_email_check.py

--- .github/scm_scripts/pusher_email_check.py
+++ .github/scm_scripts/pusher_email_check.py
@@ -5,10 +5,10 @@ import re
 import sh
 from git import Repo
  
-regex = "^[a-zA-Z0-9].@infoblox.com$"
+regex = "^[a-zA-Z0-9]*.@infoblox.com$"
 
-def check_author_email(email, author):
-    if (re.fullmatch(regex, author_email)):
+def check_author_email(email):
+    if (re.fullmatch(regex, email)):
         print("VALID DOMAIN NAME")
         sys.exit(0)
         
@@ -18,17 +18,20 @@ def check_author_email(email, author):
 
     
 if __name__ == '__main__':
-    repo = sh.git.bake(_cwd= '/mnt/home/sy/Hello-World')
-    commits_list = list(repo.iter_commits())
+    #repo = sh.git.bake(_cwd= '/mnt/home/sy/Hello-World') 
+    repo= Repo('../../')
+    commits_list = list(repo.iter_commits(max_count=1))
 
-    commit = commits_list[1]
-
+    commit = commits_list[0]
+    '''
     author = repo.git.log( "--pretty='%ae'", "-n 1")
     print(author.email)
-    author_email = author.email 
+    author_email = author.email
     if len(sys.argv) < 2:
         print("Usage: email_check <pusher email>")
         sys.exit(1)
 
     author_email = sys.argv[1]
-    check_author_email(email, author)
+    '''
+    email = commit.author.email
+    check_author_email(email)
