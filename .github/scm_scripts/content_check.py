a0ea0d84d0cd83086f65ee66567273a15ca7ac64
SuhailY14 <109067113+SuhailY14@users.noreply.github.com>
2022-07-27 14:49:00 +0530

    Update content_check.py

--- .github/scm_scripts/content_check.py
+++ .github/scm_scripts/content_check.py
@@ -15,11 +15,7 @@ def get_file_content(content):
         
 if __name__ == '__main__':
     repo = Repo('../../test.txt')
-<<<<<<< HEAD
     # response = urllib.request.urlopen("https://github.com/SuhailY14/Hello-World/blob/BLDTLS-137/test.txt")
-=======
-    #response = urllib.request.urlopen("https://github.com/SuhailY14/Hello-World/blob/BLDTLS-137/test.txt")
->>>>>>> 1362bd4880f09d871632eb2aaa219937b4b8b5d1
     content = response.read()
     content = content.decode("utf-8") 
         
