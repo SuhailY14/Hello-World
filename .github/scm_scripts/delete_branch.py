0c79587f16b5411362efed4a97cbfbd023877c1a
SuhailY14 <suhailtals28@gmail.com>
2022-07-27 02:07:41 -0700

    BLDTLS-127 : To delete a branch automatically after Merge completely

--- .github/scm_scripts/delete_branch.py
+++ .github/scm_scripts/delete_branch.py
@@ -4,9 +4,9 @@ import sys
 
 def get_merged_branches():
     ''' a list of merged branches, not couting the current branch or main '''
-    raw_results = check_output('git branch --merged upstream/main', shell=True)
-    return [b.strip() for b in raw_results.split('\n')
-        if b.strip() and not b.startswith('*') and b.strip() != 'main']
+    raw_results = check_output('git branch --merged origin', shell=True)
+    return [b.strip() for b in raw_results.split(b'\n')
+        if b.strip() and not b.startswith(b'*') and b.strip() != 'main']
 
 
 def delete_branch(branch):
@@ -17,10 +17,10 @@ if __name__ == '__main__':
     dry_run = '--confirm' not in sys.argv
     for branch in get_merged_branches():
         if dry_run:
-            print branch
+            print(branch)
         else:
-            print delete_branch(branch)
+            print(delete_branch(branch))
     if dry_run:
-        print '*****************************************************************'
-        print 'Did not actually delete anything yet, pass in --confirm to delete'
-        print '*****************************************************************'
+        print('*****************************************************************')
+        print('Did not actually delete anything yet, pass in --confirm to delete')
+        print('*****************************************************************')
