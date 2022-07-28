13cab4665545de22ee979379cb2b524409f0de2e
SuhailY14 <suhailtals28@gmail.com>
2022-07-28 02:14:31 -0700

    BLDTLS-216 : To check for non_ascii characters in text files

--- .github/scm_scripts/content_check.py
+++ .github/scm_scripts/content_check.py
@@ -14,12 +14,30 @@ def get_file_content(content):
         print("VALID CONTENT")
         
 if __name__ == '__main__':
+    filepath = '../../test.txt'
+    with open(filepath) as fp:
+        line = fp.readline()
+        cnt = 1
+        while line:
+            print("Line {}: {}".format(cnt, line.strip()))
+            line = fp.readline()
+            cnt += 1
+    
+    content = line
+    get_file_content(content)
+
+    '''
+    sys.path.append('../../test.txt')
     repo = Repo('../../test.txt')
     #response = urllib.request.urlopen("https://github.com/SuhailY14/Hello-World/blob/BLDTLS-137/test.txt")
-    content = response.read()
+    #from 'Hello-World' import test.txt
+
+
+    content = r.read()
     content = content.decode("utf-8") 
         
 
     content = commit.file.content
-    get_file_content(content)
+    '''
+    #get_file_content(content)
 
